ea9b4cd6a77fc4983a7fca802634ff5e69e92273
PW <przemek.wrobel@poczta.fm>
2021-10-21 09:39:17 +0200

    Add ; na końcu linii

--- M3.DDL.ZadTeorSQL.sql
+++ M3.DDL.ZadTeorSQL.sql
@@ -1,6 +1,6 @@
-create schema training
+create schema training;
 
-alter schema training rename to training_zs
+alter schema training rename to training_zs;
 
 
 create table training_zs.products (
@@ -10,11 +10,11 @@ product_name varchar (100),
 product_code varchar (10),
 description text,
 manufacturing_date date
-)
+);
 
-alter table training_zs.products add primary key (id)
+alter table training_zs.products add primary key (id);
 
-drop table if exists products
+drop table if exists training_zs.products;
 
 create table training_zs.sales (
 id int primary key,
@@ -24,8 +24,8 @@ sales_qty numeric (10, 2),
 product_id int,
 added_by text default ('admin'),
 constraint sales_over_1k check (sales_amount > 1000)
-)
+);
 
-alter table training_zs.sales add foreign key (product_id) references training_zs.products(id) on delete cascade
+alter table training_zs.sales add foreign key (product_id) references training_zs.products(id) on delete cascade;
 
-drop schema training_zs cascade
+drop schema training_zs cascade;
