2523fcf7afc3bf404652362c5d8281e8337f4edf
PW <przemek.wrobel@poczta.fm>
2021-12-20 14:20:40 +0100

    --4

--- M3.DDL.Projekt.sql
+++ M3.DDL.Projekt.sql
@@ -1,6 +1,6 @@
 DROP SCHEMA IF EXISTS expence_tracker CASCADE ;
--- Usuwanie schematu w opcji CASCADE w sumie nie wiem jak zadzia³a to polecenie je¿eli by dane by³y
--- pobierane dopowi¹zanych  tabel w tym lub innym schemacie
+-- Usuwanie schematu w opcji CASCADE w sumie nie wiem jak zadziaï¿½a to polecenie jeï¿½eli by dane byï¿½y
+-- pobierane dopowiï¿½zanych  tabel w tym lub innym schemacie
 CREATE SCHEMA IF NOT EXISTS expence_tracker;
 --1
 DROP TABLE IF EXISTS expence_tracker.bank_accaunt_owner;
@@ -17,30 +17,19 @@ CREATE TABLE IF NOT EXISTS expence_tracker.bank_accaunt_owner(
 DROP TABLE IF EXISTS expence_tracker.bank_accaunt_types;
 CREATE TABLE IF NOT EXISTS expence_tracker.bank_accaunt_types(
     id_ba_type integer CONSTRAINT id_ba_type PRIMARY KEY,
-    id_ba_own integer CONSTRAINT ,
+    id_ba_own integer,
     ba_type varchar  ( 50 ) NOT NULL ,
     ba_desc varchar ( 250 ),
     active boolean NOT NULL DEFAULT TRUE,
     is_common_account boolean NOT NULL DEFAULT TRUE,
     id_ba_own integer,
     insert_date timestamp DEFAULT current_timestamp,
-    update_date timestamp DEFAULT current_timestamp
+    update_date timestamp DEFAULT current_timestamp,
+    CONSTRAINT fk_bank_accaunt_owner
+        FOREIGN KEY(id_ba_own)
+            REFERENCES expence_tracker.bank_accaunt_owner(id_ba_own)
 );
---3
-DROP TABLE IF EXISTS expence_tracker.transactions;
-CREATE TABLE IF NOT EXISTS expence_tracker.transactions(
-    id_transaction integer CONSTRAINT id_transaction PRIMARY KEY,
-    id_trans_ba integer,
-    id_trans_cat integer,
-    id_trans_subcat integer,
-    id_trans_type integer,
-    id_user integer,
-    transaction_date date DEFAULT current_date,
-    transaction_value NUMERIC (9,2),
-    transaction_description TEXT,
-    insert_date timestamp DEFAULT current_timestamp,
-    update_date timestamp DEFAULT current_timestamp
-);
+
 --4
 DROP TABLE IF EXISTS expence_tracker.transaction_bank_accaunts;
 CREATE TABLE IF NOT EXISTS expence_tracker.transaction_bank_accaunts(
@@ -52,7 +41,25 @@ CREATE TABLE IF NOT EXISTS expence_tracker.transaction_bank_accaunts(
     active boolean NOT NULL DEFAULT TRUE,
     insert_date timestamp DEFAULT current_timestamp,
     update_date timestamp DEFAULT current_timestamp
+
 );
+
+--3
+DROP TABLE IF EXISTS expence_tracker.transactions;
+CREATE TABLE IF NOT EXISTS expence_tracker.transactions(
+    id_transaction integer CONSTRAINT id_transaction PRIMARY KEY,  
+    id_trans_ba integer,
+    id_trans_cat integer,
+    id_trans_subcat integer,
+    id_trans_type integer,
+    id_user integer,
+    transaction_date date DEFAULT current_date,
+    transaction_value NUMERIC (9,2),
+    transaction_description TEXT,
+    insert_date timestamp DEFAULT current_timestamp,
+    update_date timestamp DEFAULT current_timestamp
+);
+
 
 --5
 DROP TABLE IF EXISTS expence_tracker.transaction_category;
